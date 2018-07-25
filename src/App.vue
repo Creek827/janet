286a0baba7345a0e46c3db5f7032342db37a260e
janet <262743999@qq.com>
2018-07-25 21:27:12 +0800

    项目

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>感觉自己萌萌哒</h2>
+   <router-link to="/news">大事</router-link>
+   <router-link to="/stars">明星</router-link>
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import news from './components/news.vue';
+import stars from './components/stars.vue';
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes : [
+    {path:'/news', component: news},
+    {path:'/stars', component: stars},
+  ]
+})
 Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
